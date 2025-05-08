7b18e613108b9413606ed391083259dabdc05a63
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-05-08 14:51:10 -0500

    Worked through learning about string and numeric variables
    Rather than redacting the sandbox with every new itteration I decided to keep individual sub-sandbox's for reference.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,18 +1,16 @@
-alert('Hello, World');
-
-// let allows us to create a variable.
+// let allows us to create, and define a value for a variable.
 let age = 25;
 let year = 2019;
 
 // Prints result to console.
 console.log(age, year); // The comma allows for multiple values to be retrieved.
 
-/* age = 30; // Defining a new value for an existing varible.
+age = 30; // Defining a new value for an existing varible.
 console.log(age); // When invoked again, it will display the updated value declared.
 
 // Defines an un-updatible variable, unlike above where age had the abiity to change.
 const points = 100;
-console.log(points); */
+console.log(points);
 
 var score = 75; // Legacy means of declaring a variable.
 console.log(score);
