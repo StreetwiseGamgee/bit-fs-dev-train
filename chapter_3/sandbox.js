4905d318c1a237046e95d162f0b2527357baccca
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-05-09 13:38:53 -0500

    Worked with using switch statements, and block-level scope.

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -59,8 +59,8 @@ do{
     i++;
 } while(i < 5); */
 
-// conditional statements
-/* const powerLevel = 10000;
+/* // conditional statements
+const powerLevel = 10000;
 if(powerLevel > 9000){
     console.log('The scouter indicates their power level IS OVER 9000!!!');
 }
@@ -68,16 +68,16 @@ if(powerLevel > 9000){
 const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
 if(ninjas.length > 4){ // Will NOT fire as it's not greater than four.
     console.log("That's alot of ninjas");
-} */
+}
 
-/* const password = 'user@';
+const password = 'user@';
 if(password.length >= 12 && password.includes('@')){ // logical operator: &&.
     console.log('MEETS_STRONG_INTEGRITY');
 } else if(password.length >= 8 || password.includes('@') && password.length > 5){ // logical operator: ||
     console.log('MEETS_BASIC_INTEGRITY');
 } else {
     console.log("FAILED_PASSWORD_INTEGRITY");
-} */
+}
 
 // logical NOT (!)
 let user = false;
@@ -102,4 +102,67 @@ for(let i = 0; i < scores.length; i++){
         console.log('congrats, you got the top score!');
         break; // a break will cease the for-loop's conditional firing when explicitly met.
     }
+} */
+
+// switch statements
+/* syntax: 
+ *      switch(what we're evaluating){
+ *      case 'comparative value' :
+ *              output;
+ *      case 'comparitive value' :
+ *              output;
+ *      default:
+ *              output;
+ * }
+ */
+
+const grade = 'D';
+switch(grade){
+    case 'A':
+        console.log('you got an A!');
+        break;
+    case 'B':
+        console.log('you got an B!');
+        break;
+    case 'C':
+        console.log('you got an C!');
+        break;
+    case 'D':
+        console.log('you got an D!');
+        break;
+    case 'E':
+        console.log('you got an E!');
+        break;
+    default:
+        console.log('not a valid grade');
 }
+
+// variables & block level scope
+/*
+ *  Terms:
+ *  Scope:
+ *  The area in which  a variable value is relevant.
+ * 
+ *  Globale Scope:
+ *  A variable that is defined in the root of the document...
+ *  whether using 'let' || 'const'.
+ *  
+ * NO-NO: Don't use keyword 'var' as it will utterly disregard the block scope.
+ */
+let age = 30; // Global Scope
+// let age = 40; // Cannot redeclare using 'let' inside the root.
+
+if(true){
+    // We are able to redefine a variable inside the code block.
+    // the defined local scope is accessible within it's respective code block.
+    let age = 24 + " " + "Year's old.";
+    let name = 'Alexander Supertramp' 
+    console.log('inside first code block: ', age, name);
+
+    if(true){
+        let age = 50;
+        console.log('inside 2nd code block: ', age);
+    }
+}
+
+console.log('outside code block: ', age);
