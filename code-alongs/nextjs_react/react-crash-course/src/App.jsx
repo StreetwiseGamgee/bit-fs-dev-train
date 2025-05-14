54772ed799c4482d8a10fbd09550d8c1f46d2fb8
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-05-14 16:05:27 -0500

    Worked further with state passing, implemeting modal window to open and close
    Imported assets MainHeader.jsx && MainHeader.module.css via raw github user content

--- code-alongs/nextjs_react/react-crash-course/src/App.jsx
+++ code-alongs/nextjs_react/react-crash-course/src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import PostsList from './components/PostsList';
+import MainHeader from './components/MainHeader';
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+
+  function showModalHandler(event) {
+      setModalIsVisible(true);
+  }
+
+  function hideModalHandler(event) {
+      setModalIsVisible(false);
+  }
+
   return (
-  <main>
-      <PostsList/>
-  </main> 
+  <>
+  <MainHeader onCreatePost={showModalHandler} />
+    <main>
+        <PostsList 
+            isPosting={modalIsVisible} 
+            onStopPosting={hideModalHandler}
+        />
+    </main> 
+  </>
   );
 }
 export default App;
