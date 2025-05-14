9208e4b0c57e89abe2a8b59eabd0aad82f73f991
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-05-14 11:28:54 -0500

    Implemented framework for creating a new post, and tested event listening

--- code-alongs/nextjs_react/react-crash-course/src/components/PostsList.jsx
+++ code-alongs/nextjs_react/react-crash-course/src/components/PostsList.jsx
@@ -1,12 +1,20 @@
 import Post from './Post';
-import classes from './PostsList.module.css'
+import NewPost from './NewPost';
+import classes from './PostsList.module.css';
 
 function PostsList() {
+     {/* To meet the requirement .jsx has regarding child elements we must first import 'NewPost' and or the desired import.
+       * Next we wrap those elements inside an empty element aka 'fragment' <> </> as sibling elements aren't allowed.  
+       * This ensures that we only have one root element inside the return statement.
+       */}
     return (
-    <ul className={classes.posts}>
-        <Post author="Crispy C" body="React.js is a test of patience"/>
-        <Post author="Alexander Ocheeva" body="Don't forget too like an subscribe"/>
-    </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Crispy C" body="React.js is a test of patience"/>
+                <Post author="Alexander Ocheeva" body="Don't forget too like an subscribe"/>
+            </ul>
+        </>
     );
 }
 
