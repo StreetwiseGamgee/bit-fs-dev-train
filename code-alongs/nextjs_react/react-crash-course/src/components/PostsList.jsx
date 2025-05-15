b225db1ece07903f06e6e7e52aec89d84303cd0d
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-05-15 09:00:32 -0500

    Implemented rudimentary functionality for creating a new post, and outputting values to the console
    Moved state-slices and functions from PostsList.jsx to NewPost.jsx. Additionally refactoring, and or cleaning-up PostsList.jsx

--- code-alongs/nextjs_react/react-crash-course/src/components/PostsList.jsx
+++ code-alongs/nextjs_react/react-crash-course/src/components/PostsList.jsx
@@ -5,17 +5,7 @@ import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList({isPosting, onStopPosting}) {
-    const [enteredBody, setEnteredBody]  = useState ('');
-    const [enteredAuthor, setEnteredAuthor ] = useState('');
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
-
+    
     // let modalContent;
     // if (modalIsVisible) {
     //     modalContent = (
@@ -31,14 +21,11 @@ function PostsList({isPosting, onStopPosting}) {
         <> {/*Empty element acting as wrapper for sibling elements as they cannot be stored in root. */}
             {isPosting && ( 
                 <Modal onClose={onStopPosting}>
-                   <NewPost onBodyChange={bodyChangeHandler} 
-                           onAuthorChange={authorChangeHandler}
-                   />
+                   <NewPost onCancel={onStopPosting} />
                </Modal>
             )}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody}/>
-                <Post author="Alexander Ocheeva" body="Don't forget too like an subscribe"/>
+                <Post author="Streetwise Gamgee" body="Don't forget too like an subscribe"/>
             </ul>
         </>
     );
