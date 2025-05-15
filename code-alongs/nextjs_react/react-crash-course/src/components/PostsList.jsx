15f8ae2ace56b8594e83ad1844880d51a578c0cb
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-05-15 14:50:02 -0500

    Implemented retrieval of stored posts, modified app.js to simulate latency

--- code-alongs/nextjs_react/react-crash-course/src/components/PostsList.jsx
+++ code-alongs/nextjs_react/react-crash-course/src/components/PostsList.jsx
@@ -1,12 +1,25 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Post from './Post';
 import NewPost from './NewPost';
 import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList({isPosting, onStopPosting}) {
-const [posts, setPosts] = useState([]);
-    
+    const [posts, setPosts] = useState([]);
+    const[isFetching, setIsFetching] = useState(false);
+
+useEffect(() => {
+    async function fetchPosts() {
+        setIsFetching(true);
+        const response = await fetch('http://localhost:8080/posts')
+        const resData = await response.json();
+        setPosts(resData.posts);
+        setIsFetching(false);
+    }
+
+    fetchPosts();
+}, []);
+
 function addPostHandler(postData) {
     fetch('http://localhost:8080/posts', {
         method: 'POST',
@@ -17,24 +30,29 @@ function addPostHandler(postData) {
     });
     setPosts((existingPosts) => [postData, ...existingPosts])
 }
-    return (        
+return (        
         <> 
             {isPosting && ( 
                 <Modal onClose={onStopPosting}>
                     <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
                 </Modal>
             )}
-            {posts.length > 0 && ( // If post(s) values are > 0 the post(s) will display.
+            {!isFetching && posts.length > 0 && ( // If post(s) values are > 0 the post(s) will display.
                 <ul className={classes.posts}>
                     {posts.map((post) => (
                         <Post key={post.body} author={post.author} body={post.body} />
                     ))}
                 </ul>
             )}
-            {posts.length === 0 && ( 
-                <div style={{textAlign: 'center', color: 'white'}}>
+            {!isFetching && posts.length === 0 && ( 
+                <div style={{ textAlign: 'center', color: 'white'}}>
                     <h2>There are no posts yet!</h2>
                     <p>Start adding some!</p>
+                </div>
+            )}
+            {isFetching && ( 
+                <div style={{ textAlign: 'center', color: 'white'}}>
+                    <p>Loading posts...</p>
                 </div>
             )}
         </>
