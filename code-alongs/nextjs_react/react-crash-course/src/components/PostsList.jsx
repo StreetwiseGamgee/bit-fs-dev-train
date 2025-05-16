9f9ec4c1077024e0f555b88accf8b66c7b262b4f
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-05-16 15:32:13 -0500

    Finished react-crash-course
    Implemented data submission using react-router, and 'action:'.

--- code-alongs/nextjs_react/react-crash-course/src/components/PostsList.jsx
+++ code-alongs/nextjs_react/react-crash-course/src/components/PostsList.jsx
@@ -5,22 +5,12 @@ import classes from './PostsList.module.css';
 function PostsList() {
     const posts = useLoaderData();
 
-function addPostHandler(postData) {
-    fetch('http://localhost:8080/posts', {
-        method: 'POST',
-        body: JSON.stringify(postData),
-        headers: {
-            'Content-Type' : 'application/json'
-        }
-    });
-    setPosts((existingPosts) => [postData, ...existingPosts])
-}
 return (        
         <> 
             {posts.length > 0 && ( // If post(s) values are > 0 the post(s) will display.
                 <ul className={classes.posts}>
                     {posts.map((post) => (
-                        <Post key={post.body} author={post.author} body={post.body} />
+                        <Post key={post.id} id={post.id} author={post.author} body={post.body} />
                     ))}
                 </ul>
             )}
