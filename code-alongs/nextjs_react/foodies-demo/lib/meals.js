199cbc6ac7fbadc58f917fe5652de9c2a15abf5a
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-05-26 15:45:23 -0500

    Tested error handling, implemented not-found page, updated meals details to dyn. disp. menu details

--- code-alongs/nextjs_react/foodies-demo/lib/meals.js
+++ code-alongs/nextjs_react/foodies-demo/lib/meals.js
@@ -4,5 +4,11 @@ const db = sql('meals.db');// Execs sql as a function
 
 export async function getMeals() {
     await new Promise((resolve) => setTimeout(resolve, 2000)); // Arbit delay to simulate latentcy.
+
+    // throw new Error('Loading meals failed');
     return db.prepare('SELECT * FROM meals').all(); // Run() would be used if we we're INSERTing data, and get() if fetching a single row. 
+}
+
+export function getMeal(slug) {
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
