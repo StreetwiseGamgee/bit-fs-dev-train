1cefe45885ac4802339909d3b8a03eab0e666841
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-05-27 10:37:29 -0500

    Implemented custom image-picker input component
    Additionally added an image preview when fiiling out meal share

--- code-alongs/nextjs_react/foodies-demo/app/meals/[mealSlug]/page.js
+++ code-alongs/nextjs_react/foodies-demo/app/meals/[mealSlug]/page.js
@@ -1,9 +1,16 @@
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
 import { getMeal } from '@/lib/meals'; 
 import classes from './page.module.css';
 
 export default function MealDetailsPage({params}) {
     const meal = getMeal(params.mealSlug);
+if (!meal) {
+    // Calling this funct. will stop the component from exec.
+    notFound(); //  and will show the closest not-found error page
+}
+
     meal.instructions = meal.instructions.replace(/\n/g, '<br />');
     return(
         <>
---
+++ code-alongs/nextjs_react/foodies-demo/components/meals/image-picker.js
@@ -0,0 +1,62 @@
+'use client';
+import { useRef, useState } from 'react';
+import Image from 'next/image';
+import classes from './image-picker.module.css';
+
+export default function ImagePicker({label, name}) {
+    const [pickedImage, setPickedImage] = useState(); {/* Client Component */}
+    const imageInput = useRef();
+
+    function handlePickClicked() {
+        imageInput.current.click();
+    }
+
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+
+        if (!file) {
+            return;
+        }
+
+        // Conversion of file uploaded into a 'data url' to preview the image before submission
+        const fileReader = new FileReader();
+        fileReader.onload = () => { // Stores a funct. as a value within the onload property
+            setPickedImage(fileReader.result);            
+        }; 
+
+        fileReader.readAsDataURL(file);
+    }
+
+    return ( 
+        <div className={classes.picker}>
+            <label htmlFor={name}>{label}</label>
+            <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No Image Selected Yet...</p>}
+                    {pickedImage && (
+                        <Image 
+                            src={pickedImage} 
+                            alt="The image selected by the user uploading" 
+                            fill
+                        />
+                    )}
+                </div>
+                <input
+                    className={classes.input}
+                    type="file" 
+                    id={name}
+                    accept="image/png, image/jpeg"
+                    name={name}
+                    ref={imageInput}
+                    // multiple {*In the instance where multiple files are required.*}
+                    onChange={handleImageChange}
+                />
+            <button className={classes.button} 
+            type="button" 
+            onClick={handlePickClicked}> {/* Requires use client statement to be used... */}
+                Pick an Image
+            </button>
+        </div>
+    </div>
+    );
+}
