2a26744e4ae8a7c32bbe6b426abb5ca3eb23464b
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-05-28 15:24:13 -0500

    Added server-side input validation for users sharing a meal
    Preventing empty inputs from being processed

--- code-alongs/nextjs_react/foodies-demo/lib/actions.js
+++ code-alongs/nextjs_react/foodies-demo/lib/actions.js
@@ -2,8 +2,11 @@
 import { redirect } from 'next/navigation';
 import { saveMeal } from './meals';
 
+function isInvalidText(text) {
+    return !text || text.trim() === '';
+}
+
 export async function shareMeal(formData) {
-    // console.log(formData);
     // Form object
     const meal = {
         title: formData.get('title'),
@@ -13,7 +16,18 @@ export async function shareMeal(formData) {
         creator: formData.get('name'),
         creator_email: formData.get('email'),
     }
-  // console.log(meal);
+    if (
+        isInvalidText(meal.title) || 
+        isInvalidText(meal.summary) ||
+        isInvalidText(meal.instructions) || 
+        isInvalidText(meal.creator) ||
+        isInvalidText(meal.creator_email) ||
+        !meal.creator_email.includes('@') ||
+        !meal.image || meal.image.size === 0
+    )  {
+       throw new Error('Invalid input');
+    }
+
   await saveMeal(meal);
   redirect('/meals');
 }
